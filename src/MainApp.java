ff8f70b96ee299598313876279d754542076aa88
reakoffa <reakoffa@gmail.com>
2018-05-10 13:12:21 -0400

    bye felicia

--- src/MainApp.java
+++ src/MainApp.java
@@ -4,6 +4,8 @@ public class MainApp {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Sup world!");
+		
+		System.out.println("bye felicia");
 	}
 
 }
